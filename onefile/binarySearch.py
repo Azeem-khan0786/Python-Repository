42fd052b2f920e5c5840f7d19b6937f928f385e4
Azeem-khan0786 <ak9828129@gmail.com>
2026-04-03 00:43:40 +0530

    Update Feature

--- onefile/binarySearch.py
+++ onefile/binarySearch.py
@@ -1,8 +1,16 @@
-def binarySearch(nums):
-    pass
+def binarySearch(nums,x):
+    n=len(nums)
+    start,end=0,n-1
+    while start<=end:
+        mid=(end+start)//2
+        if nums[mid]==x:
+            return mid
+        elif nums[mid]<x:
+            start=mid+1
+        else:
+            end=mid-1
+    return -1            
 nums= [12,31,34,11]
-x=3
-binarySearch(nums)
-print(f"Search index of given element{x}")
-
-    
+x=34
+target=binarySearch(nums,x)
+print(f"Search index of given element{x}",target)
